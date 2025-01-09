45cb353f1fc5fd339072cd57d459701763dfefb5
ahmet emin <ahmetemin.tek.66@gmail.com>
2025-01-09 15:43:08 +0000

    initial commit

--- trial.py
+++ trial.py
@@ -20,7 +20,7 @@ st.set_page_config(
 
 # Sidebar
 with st.sidebar:
-    st.image("/Users/ahmetemintek/Desktop/VLLMs/ziaistudio_logo.jpeg", width=200)
+    st.image("https://raw.githubusercontent.com/yourusername/yourrepo/main/assets/ziaistudio_logo.jpeg", width=200)
     st.markdown("---")
     feature = st.selectbox(
         "Select Feature",
